3c773f4488a443ad970fed7d1aef432d2fb00101
marvinpolster <marvin.polster@gmail.com>
2020-10-15 20:28:09 +0200

    Improvements
    HORNSAT.HS
    - deleted unnecessary signum in Literal Data Structure
    - improved show instance for Formula, Literal and Clause
    - mark facts in a Formula before starting HornSatH

--- src/HORNSAT.hs
+++ src/HORNSAT.hs
@@ -1,37 +1,31 @@
 module HORNSAT (Formula (..), Literal (..), Clause (..), hornSat) where
 
-data Literal = Literal {sig::Bool, name::String} | BTM | TOP deriving (Eq)
+data Literal = Literal {name::String} | BTM | TOP deriving (Eq)
 data Clause = Clause {prec::[Literal], impl::Literal}
 data Formula = Formula {clauses::[Clause]}
 type Model = [Literal]
 
 instance Show Literal where 
-    show = showLiteral
+    show (Literal x) = x
+    show x = if x == TOP then "⊤" else "⊥"
 
 instance Show Clause where 
-    show (Clause p i) = (init (foldl (++) "" (map (\x -> (show x) ++ "∧") p))) ++ "->" ++ (show i)
+    show (Clause p i) = init (foldl (++) "" (map (\x -> show x ++ "∧") p)) ++ "->" ++ show i
 
 instance Show Formula where 
-    show = showFormula
+    show (Formula c) = init (foldl (++) "" (map (\x -> "(" ++ show x ++ ")∧") c))
 
-showLiteral :: Literal -> String
-showLiteral TOP = "⊤"
-showLiteral BTM = "⊥"
-showLiteral (Literal s n) = (if s then "" else "¬") ++ n
-
-showFormula :: Formula -> String
-showFormula f = init (foldl (++) "" (map(\x -> "(" ++ (show x) ++ ")∧") (clauses f)))
-
-hornSat :: Formula -> String
-hornSat f = case (hornSatH f 0 []) of 
-    Just modell -> show modell
-    Nothing -> "Not Satisfiable"
+hornSat :: Formula -> (String, Model)
+hornSat f = case hornSatH f 0 (map (\x-> prec x !! 0) (filter (\s -> impl s == TOP) (clauses f))) of 
+    Just model -> if model == [BTM] then ("Invalid Formula!", []) else ("Satisfiable!", model)
+    Nothing -> ("Not Satisfiable!", [])
 
 hornSatH :: Formula -> Int -> Model -> Maybe Model
 hornSatH f n acc 
-    | n == length (clauses f) = Just acc
-    | null (prec ((clauses f) !! n)) = Nothing
-    | impl ((clauses f) !! n) == TOP && (prec ((clauses f) !! n) !! 0) `notElem` acc = hornSatH f 0 (((prec ((clauses f) !! n)) !! 0):acc)
-    | foldl (&&) True ((map (`elem` acc) (prec ((clauses f) !! n)))) && (impl ((clauses f) !! n)) == BTM = Nothing
-    | foldl (&&) True ((map (`elem` acc) (prec ((clauses f) !! n)))) && (impl ((clauses f)!! n)) `notElem` [BTM, TOP] && (impl ((clauses f) !! n)) `notElem` acc = hornSatH f 0 ((impl ((clauses f) !! n)):acc)
+    | length cls == n = Just acc
+    | null (prec cls_n) = Just [BTM]
+    | foldl (&&) True (map (`elem` acc) (prec cls_n)) && (impl cls_n) == BTM = Nothing
+    | foldl (&&) True (map (`elem` acc) (prec cls_n)) && (impl cls_n) `notElem` [BTM, TOP] ++ acc = hornSatH f 0 (impl cls_n : acc)
     | otherwise = hornSatH f (n+1) acc
+    where cls = clauses f
+          cls_n = cls !! n
--- src/test.hs
+++ src/test.hs
@@ -1,7 +1,9 @@
 import HORNSAT
 
 main = do
-    putStrLn (hornSat (Formula [Clause [Literal True "A", Literal True "B"] BTM, Clause [Literal True "B"] TOP, Clause [Literal True "C", Literal True "B"] (Literal True "B")]))
-    putStrLn (hornSat (Formula [Clause [Literal True "A2", Literal True "A4"] (Literal True "A3"), Clause [Literal True "A4", Literal True "A1"] (Literal True "A2"), Clause [Literal True "A4"] (Literal True "A1"), Clause [Literal True "A3"] BTM]))
-    putStrLn (hornSat (Formula []))
-    putStrLn $ show (Formula [Clause [Literal True "A", Literal True "B"] BTM, Clause [Literal True "B"] TOP, Clause [Literal True "C", Literal True "B"] (Literal True "B")])
+    putStrLn $ show (hornSat (Formula [Clause [Literal "A", Literal "B"] BTM, Clause [Literal "B"] TOP, Clause [Literal "C", Literal "B"] (Literal "B")]))
+    putStrLn $ show (hornSat (Formula [Clause [Literal "A2", Literal "A4"] (Literal "A3"), Clause [Literal "A4", Literal "A1"] (Literal "A2"), Clause [Literal "A4"] (Literal "A1"), Clause [Literal "A3"] BTM]))
+    putStrLn $ show (hornSat (Formula [Clause [Literal "A1", Literal "A6"] (Literal "A3"), Clause [Literal "A2"] TOP, Clause [Literal "A3", Literal "A4"] (Literal "A2"), Clause [Literal "A2"] (Literal "A1"), Clause [Literal "A5", Literal "A1"] (Literal "A7"), Clause [Literal "A6"] TOP, Clause [Literal "A3", Literal "A4"] BTM]))
+    putStrLn $ show (hornSat (Formula []))
+    putStrLn $ show (hornSat (Formula [Clause [Literal "B"] BTM, Clause [Literal "B"] TOP]))
+    putStrLn $ show (Formula [Clause [Literal "A", Literal "B"] BTM, Clause [Literal "B"] TOP, Clause [Literal "C", Literal "B"] (Literal "B")])
